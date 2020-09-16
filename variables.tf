18b240a934a0a6b71cb7c4b48be4a6256a7960c9
Arun <arun.surya55@gmail.com>
2020-09-16 19:16:22 +0530

    aks scrip

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "cluster_name" {
+
+}
+
+variable "resource_group_name" {
+
+}
+
+variable "environment" {
+
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "node_count" {
+  default = 3
+}
+
+variable "node_size" {
+  default = "Standard_D2_v2"
+}
+
+variable "service_principal" {
+  type = object({
+    client_id     = string
+    client_secret = string
+  })
+  description = "The service principal to use"
+}
+
+variable dns_prefix {
+}
